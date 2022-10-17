e5b9c25eefa7c5d5156a60da343214d1d8b038bb
Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>
2022-10-17 14:07:55 +0800

    Use names instead of numbers for naming layers
    Signed-off-by: Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>

--- keyboards/jsa/pragmatic34/v3/keymaps/jesse/keymap.c
+++ keyboards/jsa/pragmatic34/v3/keymaps/jesse/keymap.c
@@ -34,56 +34,47 @@
 #define ZOOMOUT C(KC_PMNS)
 #define ACTUAL C(KC_0)
 
+enum my_layers {
+    COLEMAK = 0,
+    NUM,
+    SYM,
+    WM,
+    FUNC,
+    QWERTY,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [0] = LAYOUT(
+    [COLEMAK] = LAYOUT(
              KC_Q,     KC_W,     KC_F,     KC_P, KC_B, KC_J,     KC_L,     KC_U,     KC_Y,  KC_SCLN,
         _LG(KC_A),_LA(KC_R),_LC(KC_S),_LS(KC_T), KC_G, KC_M,_LS(KC_N),_LC(KC_E),_LA(KC_I),_LG(KC_O),
              KC_Z,     KC_X,     KC_C,     KC_D, KC_V, KC_K,     KC_H,  KC_COMM,   KC_DOT,  KC_SLSH,
-                                  TG(4), LT(1,KC_SPC),LT(2,KC_BSPC), LT(3,KC_ENT)
+                           TG(QWERTY), LT(NUM,KC_SPC),LT(SYM,KC_BSPC), LT(WM,KC_ENT)
     ),
-    [1] = LAYOUT(
+    [NUM] = LAYOUT(
         KC_ESC,  _______, _______,  KC_UNDS, KC_DQUO, KC_PLUS, KC_7,   KC_8,   KC_9,   KC_PERC,
         KC_TAB,  CUT,     COPY,      PSTE,   KC_QUOT,  KC_MINS, KC_4,   KC_5,   KC_6,   KC_ASTR,
         KC_CAPS,ZOOMIN, ZOOMOUT,   ACTUAL,  KC_EQL,  KC_DLR,  KC_1,   KC_2,   KC_3,   KC_SLSH,
                                      _______, XXXXXXX, KC_0,   KC_DOT
     ),
-    [2] = LAYOUT(
+    [SYM] = LAYOUT(
         KC_CIRC, KC_GRV,  KC_TILD, KC_LCBR, KC_RCBR, _______,  KC_PGUP, KC_UP,   _______, _______,
         KC_EXLM, KC_AT,   KC_HASH, KC_LPRN, KC_RPRN, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
         KC_BSLS, KC_AMPR, KC_PIPE, KC_LBRC, KC_RBRC, _______, KC_PGDN, _______, _______, _______,
                                    _LG(KC_INS), _LA(KC_DEL),  XXXXXXX, _______
     ),
-    [3] = LAYOUT(
+    [WM] = LAYOUT(
         _______, G(KC_7), G(KC_8), G(KC_9), G(KC_MINS), _______, _______, G(KC_UP), _______, _______,
         KC_LSFT, G(KC_4), G(KC_5), G(KC_6), G(KC_0), _______, G(KC_LEFT), G(KC_DOWN), G(KC_RGHT), _______,
         _______, G(KC_1), G(KC_2), G(KC_3), _______, _______, _______, _______, _______, _______,
                                    KC_VOLD, KC_VOLU, _______, XXXXXXX
     ),
-    [4] = LAYOUT(
+    [QWERTY] = LAYOUT(
              KC_Q,     KC_W,     KC_E,     KC_R, KC_T, KC_Y,     KC_U,     KC_I,     KC_O,  KC_P,
         _LG(KC_A),_LA(KC_S),_LC(KC_D),_LS(KC_F), KC_G, KC_H,_LS(KC_J),_LC(KC_K),_LA(KC_L),_LG(KC_SCLN),
              KC_Z,     KC_X,     KC_C,     KC_V, KC_B, KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,
                                   _______, KC_SPC,KC_BSPC, KC_ENT
     ),
-    [5] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, _______
-    ),
-    [6] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, _______
-    ),
-    [7] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, _______
-    )
 };
 
 void keyboard_post_init_user(void) {
