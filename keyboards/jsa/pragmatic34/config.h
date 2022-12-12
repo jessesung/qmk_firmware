1d5f1165295d68fde09234705fbc221f722bbc77
Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>
2022-12-12 20:39:52 +0800

    Fixes for warnings after rebasing to 0.19.1
    Signed-off-by: Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>

--- keyboards/jsa/pragmatic34/config.h
+++ keyboards/jsa/pragmatic34/config.h
@@ -19,14 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x1209
-#define PRODUCT_ID   0xA805
-#define DEVICE_VER   0x0410
-#define MANUFACTURER Pragmatic Inc.
-#define PRODUCT      Pragmatic 34
-#define USB_MAX_POWER_CONSUMPTION 80
-
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 10
