7ac85344b478b83665ac8007feac2c90bd6d51fe
Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>
2023-03-26 21:42:11 +0800

    ferris: Add a keymap for my use case
    Signed-off-by: Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>

---
+++ keyboards/ferris/keymaps/jesse/rules.mk
@@ -0,0 +1,8 @@
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+COMBO_ENABLE = yes
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+QMK_SETTINGS = no
+TAP_DANCE_ENABLE = yes
+
+EXTRAFLAGS += --param=min-pagesize=0
