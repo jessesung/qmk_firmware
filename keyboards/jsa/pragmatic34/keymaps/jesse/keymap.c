b0ea8416b026462164c8bc974344250a84b6e5a2
Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>
2023-11-01 16:21:42 +0800

    jsa/pragmatic34: keymaps: jesse: rename keymap
    Signed-off-by: Wen-chien Jesse Sung <jesse@cola.voip.idv.tw>

--- keyboards/jsa/pragmatic34/keymaps/jesse/keymap.c
+++ keyboards/jsa/pragmatic34/keymaps/jesse/keymap.c
@@ -13,10 +13,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "action_layer.h"
+#include "keycodes.h"
+#include "keymap_us.h"
 #include QMK_KEYBOARD_H
-
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
 
 #define _LG(X) LGUI_T(X)
 #define _LA(X) LALT_T(X)
@@ -27,61 +27,140 @@
 #define _RC(X) RCTL_T(X)
 #define _RS(X) RSFT_T(X)
 
+#define REDO C(KC_Y)
 #define COPY C(KC_C)
 #define PSTE C(KC_V)
 #define CUT C(KC_X)
-#define ZOOMIN C(KC_PLUS)
-#define ZOOMOUT C(KC_PMNS)
-#define ACTUAL C(KC_0)
+#define UNDO C(KC_Z)
+
+enum {
+    U_TD_BOOT,
+};
+
+static void u_td_fn_boot(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 2)
+        reset_keyboard();
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [U_TD_BOOT] = ACTION_TAP_DANCE_FN(u_td_fn_boot),
+};
+
+enum custom_keycodes {
+    IME = SAFE_RANGE,
+};
 
 enum my_layers {
     COLEMAK = 0,
     QWERTY,
+    NAV,
+    MOUSE,
+    MEDIA,
     NUM,
     SYM,
-    WM,
     FUNC,
+    TOTAL_LAYER_COUNT,
+};
+
+const char *layer_names[TOTAL_LAYER_COUNT] = {
+    [COLEMAK] = "COLEMAK",
+    [QWERTY]  = "QWERTY",
+    [NAV]     = "NAV",
+    [MOUSE]   = "MOUSE",
+    [MEDIA]   = "MEDIA",
+    [NUM]     = "NUM",
+    [SYM]     = "SYMBOL",
+    [FUNC]    = "FUNCTION",
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
     [COLEMAK] = LAYOUT(
-             KC_Q,     KC_W,     KC_F,     KC_P, KC_B, KC_J,     KC_L,     KC_U,     KC_Y,  KC_SCLN,
-        _LG(KC_A),_LA(KC_R),_LC(KC_S),_LS(KC_T), KC_G, KC_M,_LS(KC_N),_LC(KC_E),_LA(KC_I),_LG(KC_O),
-             KC_Z,     KC_X,     KC_C,     KC_D, KC_V, KC_K,     KC_H,  KC_COMM,   KC_DOT,  KC_SLSH,
-                      LT(FUNC,KC_TAB), LT(NUM,KC_SPC),LT(SYM,KC_BSPC), LT(WM,KC_ENT)
-    ),
-    [NUM] = LAYOUT(
-        KC_ESC,  _______, _______,  KC_UNDS, KC_DQUO, KC_PLUS, KC_7,   KC_8,   KC_9,   KC_PERC,
-        _______,  CUT,     COPY,      PSTE,   KC_QUOT,  KC_MINS, KC_4,   KC_5,   KC_6,   KC_ASTR,
-        KC_CAPS,ZOOMIN, ZOOMOUT,   ACTUAL,  KC_EQL,  KC_DLR,  KC_1,   KC_2,   KC_3,   KC_SLSH,
-                                     _______, XXXXXXX, KC_0,   KC_DOT
-    ),
-    [SYM] = LAYOUT(
-        KC_CIRC, KC_GRV,  KC_TILD, KC_LCBR, KC_RCBR, _______,  KC_PGUP, KC_UP,   _______, _______,
-        KC_EXLM, KC_AT,   KC_HASH, KC_LPRN, KC_RPRN, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,
-        KC_BSLS, KC_AMPR, KC_PIPE, KC_LBRC, KC_RBRC, _______, KC_PGDN, _______, _______, _______,
-                                   _LG(KC_INS), _LA(KC_DEL),  XXXXXXX, _______
-    ),
-    [WM] = LAYOUT(
-        _______, G(KC_7), G(KC_8), G(KC_9), G(KC_MINS), _______, _______, G(KC_UP), _______, _______,
-        KC_LSFT, G(KC_4), G(KC_5), G(KC_6), G(KC_0), _______, G(KC_LEFT), G(KC_DOWN), G(KC_RGHT), _______,
-        _______, G(KC_1), G(KC_2), G(KC_3), _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, XXXXXXX
-    ),
-    [FUNC] = LAYOUT(
-        TG(QWERTY), _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-           _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-           KC_VOLD, KC_VOLU, _______, _______, _______,  _______, _______, _______, _______, _______,
-                                      XXXXXXX, _______, C(KC_SPC), _______
+        KC_Q, KC_W, KC_F, KC_P, KC_B,
+            KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
+        _LG(KC_A), _LA(KC_R), _LC(KC_S), _LS(KC_T), KC_G,
+            KC_M, _LS(KC_N), _LC(KC_E), _LA(KC_I), _LG(KC_O),
+        KC_Z, KC_X, KC_C, KC_D, KC_V,
+            KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,
+        LT(NAV, KC_TAB), LT(MOUSE, KC_SPC), LT(SYM, KC_BSPC), LT(NUM, KC_ENT)
     ),
     [QWERTY] = LAYOUT(
-             KC_Q,     KC_W,     KC_E,     KC_R, KC_T, KC_Y,     KC_U,     KC_I,     KC_O,  KC_P,
-        _LG(KC_A),_LA(KC_S),_LC(KC_D),_LS(KC_F), KC_G, KC_H,_LS(KC_J),_LC(KC_K),_LA(KC_L),_LG(KC_SCLN),
-             KC_Z,     KC_X,     KC_C,     KC_V, KC_B, KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,
-                               _______, _______, _______, _______
+        KC_Q, KC_W, KC_E, KC_R, KC_T,
+            KC_Y, KC_U, KC_I, KC_O, KC_P,
+        _LG(KC_A), _LA(KC_S), _LC(KC_D), _LS(KC_F), KC_G,
+            KC_H, _LS(KC_J), _LC(KC_K), _LA(KC_L), _LG(KC_SCLN),
+        KC_Z, KC_X, KC_C, KC_V, KC_B,
+            KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+        _______, _______, _______, _______
+    ),
+    [NAV] = LAYOUT(
+        TG(QWERTY), IME, XXXXXXX, XXXXXXX, XXXXXXX,
+            REDO, PSTE, COPY, CUT, UNDO,
+        KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,
+            XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
+        CW_TOGG, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX,
+            KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+        XXXXXXX, XXXXXXX, KC_BSPC, KC_ENT
+    ),
+    [MOUSE] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            REDO, PSTE, COPY, CUT, UNDO,
+        KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,
+            XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            KC_BTN3, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
+        XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2
+    ),
+    [MEDIA] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,
+            XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, KC_MSTP, KC_MPLY, KC_MUTE, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+    [NUM] = LAYOUT(
+        KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_QUOT, KC_4, KC_5, KC_6, KC_EQL,
+            XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_GRV, KC_1, KC_2, KC_3, KC_BSLS,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_0, KC_MINS, XXXXXXX, XXXXXXX
+    ),
+    [SYM] = LAYOUT(
+        KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_DQUO, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,
+            XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX
+    ),
+    [FUNC] = LAYOUT(
+        KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TD(U_TD_BOOT),
+        KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,
+            XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_F10, KC_MINS, XXXXXXX, XXXXXXX
     ),
 };
+
+const uint16_t PROGMEM thumbcombos_base_left[] = {LT(NAV, KC_TAB), LT(MOUSE, KC_SPC), COMBO_END};
+const uint16_t PROGMEM thumbcombos_base_right[] = {LT(SYM, KC_BSPC), LT(NUM, KC_ENT), COMBO_END};
+const uint16_t PROGMEM thumbcombos_nav[] = {KC_BSPC, KC_ENT, COMBO_END};
+const uint16_t PROGMEM thumbcombos_sym[] = {KC_LPRN, KC_RPRN, COMBO_END};
+const uint16_t PROGMEM thumbcombos_func[] = {KC_SPC, KC_TAB, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(thumbcombos_base_left, LT(MEDIA, KC_ESC)),
+    COMBO(thumbcombos_base_right, LT(FUNC, KC_DEL)),
+    COMBO(thumbcombos_nav, KC_DEL),
+    COMBO(thumbcombos_sym, KC_UNDS),
+    COMBO(thumbcombos_func, KC_APP),
+};
+const uint16_t COMBO_LEN = sizeof(key_combos) / sizeof(key_combos[0]);
 
 void keyboard_post_init_user(void) {
     // Customise these values to desired behaviour
@@ -120,6 +199,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 bool oled_task_user(void) {
     if (!oled_needs_update) return false;
     oled_needs_update = false;
+    oled_clear();
 
     // render caps lock indicator
     if (caps_lock) {
@@ -130,15 +210,23 @@ bool oled_task_user(void) {
 
     oled_set_cursor(0, 1);
 
-    if (layer == 0) {
-        oled_write_P(PSTR("-= 34 =-"), false);
-    } else {
-        char layer_indicator[] = "--------";
-        layer_indicator[layer] = layer + 48; // ex. ---3---
-        oled_write(layer_indicator, false);
-        layer_indicator[layer] = '-'; // reset indicator
-    }
+    const char *layer_name = layer_names[layer];
+    if (layer_name)
+        oled_write(layer_name, false);
     return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case IME:
+            if (record->event.pressed) {
+                layer_invert(QWERTY);
+                SEND_STRING(SS_LCTL(" "));
+            }
+            return false;
+            break;
+    }
+    return true;
 }
 
 #endif // OLED_ENABLE
